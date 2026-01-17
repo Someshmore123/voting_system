740b2852bd68ff07988abbdd57b9e900b099e785
Someshmore123 <someshmore2995@gmail.com>
2026-01-17 17:00:38 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,34 +1,12 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// Define the MongoDB connection URL
-const mongoURL = process.env.MONGODB_URL_LOCAL // Replace 'mydatabase' with your database name
-// const mongoURL = process.env.MONGODB_URL;
+console.log('RAW ENV VALUE:', process.env.MONGODB_URL_LOCAL);
+console.log('TYPE:', typeof process.env.MONGODB_URL_LOCAL);
 
-// Set up MongoDB connection
+const mongoURL = process.env.MONGODB_URL_LOCAL;
+
 mongoose.connect(mongoURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
-
-// Get the default connection
-// Mongoose maintains a default connection object representing the MongoDB connection.
-const db = mongoose.connection;
-
-// Define event listeners for database connection
-
-db.on('connected', () => {
-    console.log('Connected to MongoDB server');
 });
-
-db.on('error', (err) => {
-    console.error('MongoDB connection error:', err);
-});
-
-db.on('disconnected', () => {
-    console.log('MongoDB disconnected');
-});
-
-// Export the database connection
-module.exports = db;
-
